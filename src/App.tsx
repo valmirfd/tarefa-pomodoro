96503e38f3708fd2b974d8bf93c170e404262869
Valmir Dornas <fernandocursos2021@gmail.com>
2025-06-28 11:21:22 -0300

    Organizando pastas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import './styles/theme.css';
+import './styles/global.css';
+
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
+
 export function App() {
   return (
     <>
-      <h1>Jesus te ama!</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos,
-        necessitatibus.
-      </p>
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
+
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
     </>
   );
 }
