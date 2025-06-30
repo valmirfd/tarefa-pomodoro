c080298b31bbb2536155801952da9f60df0567c3
Valmir Dornas <fernandocursos2021@gmail.com>
2025-06-30 11:57:09 -0300

    Componente footer

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 
 // https://chronos-pomodoro-private.vercel.app/settings/
 export function App() {
@@ -49,6 +50,10 @@ export function App() {
             <DefaultButton icon={<PlayCircleIcon />} />
           </div>
         </form>
+      </Container>
+
+      <Container>
+        <Footer />
       </Container>
     </>
   );
--- src/components/DefaultButton/index.tsx
+++ src/components/DefaultButton/index.tsx
@@ -1,12 +1,17 @@
 import styles from './styles.module.css';
 type DefaultButtonProps = {
   icon: React.ReactNode;
+  color?: 'green' | 'red';
 } & React.ComponentProps<'button'>;
 
-export function DefaultButton({ icon, ...props }: DefaultButtonProps) {
+export function DefaultButton({
+  icon,
+  color = 'green',
+  ...props
+}: DefaultButtonProps) {
   return (
     <>
-      <button className={styles.button} {...props}>
+      <button className={`${styles.button} ${styles[color]}`} {...props}>
         {icon}
       </button>
     </>
