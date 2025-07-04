4264208503f8fdbdea9c3c613ddccfca8918bffb
Valmir Dornas <fernandocursos2021@gmail.com>
2025-07-04 19:52:29 -0300

    Modularizando o contexts

--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -1,4 +1,4 @@
-import { useTaskContext } from '../../contexts/TaskContext';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import styles from './styles.module.css';
 
 export function CountDown() {
