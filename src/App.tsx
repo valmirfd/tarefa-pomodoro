03c2921ca6a88418b892914c4a3409072d4c4b29
Valmir Dornas <fernandocursos2021@gmail.com>
2025-07-04 07:14:57 -0300

    HTML generic

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Home } from './pages/Home';
 import './styles/theme.css';
 import './styles/global.css';
 
-// https://chronos-pomodoro-private.vercel.app/settings/
 export function App() {
   return <Home />;
 }
